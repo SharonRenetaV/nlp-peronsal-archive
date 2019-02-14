a1452fd91f5fda7ad995c0c52d71c3e8847ffdcd
SharonRenetaV <4756+SharonRenetaV@users.noreply.github.com>
2019-02-14 18:41:04 +0530

    as2 added

--- as2.py
+++
@@ -1,7 +0,0 @@
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-pss= PorterStemmer()
-sentence = ["drivers drived the car fast"]
-words=word_tokenize(sentence)
-for w in words:
-    print (w,"        "),pss.stem(w))
