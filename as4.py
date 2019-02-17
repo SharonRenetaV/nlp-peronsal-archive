fff361069942e24316aec364e9338a82eb4310e3
SharonRenetaV <4756+SharonRenetaV@users.noreply.github.com>
2019-02-17 21:26:39 +0530

    as4 added

---
+++ as4.py
@@ -0,0 +1,9 @@
+# a function to get all the pos_tags
+
+# import nltk
+# nltk.download('punkt')
+# nltk.download('maxent_treebank_pos_tagger')
+
+from nltk import pos_tag
+sentence = ('the quick white tiger jumps over the dozzy deer')
+nltk.pos_tag('sentence')
